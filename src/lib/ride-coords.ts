cd1190541079818fefe7c7026d652df472d405d6
Todd Trevisan <mrtoddles11@gmail.com>
2026-03-21 16:22:22 -0700

    Fix Ride Coordinates UUID Mapping

--- src/lib/ride-coords.ts
+++ src/lib/ride-coords.ts
@@ -1,51 +1,177 @@
 export const RIDE_COORDS: Record<string, { lat: number, lng: number, name: string }> = {
-  // Disneyland Park
-  "7340550b-c14d-4def-80bb-acdb51d49a66": { lat: 33.8121, lng: -117.9190, name: "Disneyland Park" },
-  "3413": { lat: 33.8152, lng: -117.9197, name: "Space Mountain" },
-  "3357": { lat: 33.8123, lng: -117.9221, name: "Indiana Jones Adventure" },
-  "3381": { lat: 33.8129, lng: -117.9216, name: "Jungle Cruise" },
-  "3395": { lat: 33.8143, lng: -117.9174, name: "Matterhorn Bobsleds" },
-  "17941": { lat: 33.8130, lng: -117.9170, name: "Star Tours" },
-  "19503": { lat: 33.8157, lng: -117.9238, name: "Rise of the Resistance" },
-  "19305": { lat: 33.8150, lng: -117.9230, name: "Millennium Falcon: Smugglers Run" },
-  "3309": { lat: 33.8120, lng: -117.9230, name: "Big Thunder Mountain Railroad" },
-  "3311": { lat: 33.8124, lng: -117.9209, name: "Pirates of the Caribbean" },
-  "3348": { lat: 33.8113, lng: -117.9215, name: "Haunted Mansion" },
-  "3353": { lat: 33.8145, lng: -117.9178, name: "It's a Small World" },
-  "3329": { lat: 33.8138, lng: -117.9185, name: "Dumbo the Flying Elephant" },
-  "3325": { lat: 33.8135, lng: -117.9182, name: "Peter Pan's Flight" },
-  "3393": { lat: 33.8133, lng: -117.9220, name: "Splash Mountain" },
-  "3364": { lat: 33.8136, lng: -117.9170, name: "Alice in Wonderland" },
-  "3319": { lat: 33.8137, lng: -117.9189, name: "Casey Jr. Circus Train" },
-  "3372": { lat: 33.8133, lng: -117.9194, name: "Mad Tea Party" },
-  "3337": { lat: 33.8130, lng: -117.9180, name: "Mr. Toad's Wild Ride" },
-  "3343": { lat: 33.8131, lng: -117.9182, name: "Snow White's Enchanted Wish" },
-  "3332": { lat: 33.8134, lng: -117.9180, name: "Pinocchio's Daring Journey" },
-  "3335": { lat: 33.8140, lng: -117.9180, name: "Storybook Land Canal Boats" },
-  "3355": { lat: 33.8130, lng: -117.9175, name: "Buzz Lightyear Astro Blasters" },
-  "3334": { lat: 33.8130, lng: -117.9165, name: "Autopia" },
-  "3312": { lat: 33.8150, lng: -117.9170, name: "Finding Nemo Submarine Voyage" },
-  "3401": { lat: 33.8105, lng: -117.9190, name: "Main Street Vehicles" },
-  "3340": { lat: 33.8115, lng: -117.9195, name: "Disneyland Railroad" },
-
-  // DCA
-  "56": { lat: 33.8061, lng: -117.9190, name: "Disney California Adventure" },
-  "0df49a66-7340-4def-80bb-acdb51d49a66": { lat: 33.8061, lng: -117.9190, name: "Disney California Adventure" },
-  "19307": { lat: 33.8055, lng: -117.9215, name: "Radiator Springs Racers" },
-  "17849": { lat: 33.8045, lng: -117.9195, name: "Guardians of the Galaxy – Mission: BREAKOUT!" },
-  "19500": { lat: 33.8065, lng: -117.9195, name: "WEB SLINGERS: A Spider-Man Adventure" },
-  "17855": { lat: 33.8040, lng: -117.9220, name: "Incredicoaster" },
-  "3411": { lat: 33.8045, lng: -117.9210, name: "Toy Story Midway Mania!" },
-  "3391": { lat: 33.8035, lng: -117.9205, name: "Soarin' Around the World" },
-  "3359": { lat: 33.8050, lng: -117.9225, name: "Grizzly River Run" },
-  "19306": { lat: 33.8040, lng: -117.9230, name: "Pixar Pal-A-Round" },
-  "3405": { lat: 33.8055, lng: -117.9220, name: "The Little Mermaid ~ Ariel's Undersea Adventure" },
-  "3383": { lat: 33.8065, lng: -117.9210, name: "Monsters, Inc. Mike & Sulley to the Rescue!" },
-  "3314": { lat: 33.8040, lng: -117.9225, name: "Silly Symphony Swings" },
-  "3316": { lat: 33.8038, lng: -117.9220, name: "Goofy's Sky School" },
-  "3318": { lat: 33.8035, lng: -117.9215, name: "Jumpin' Jellyfish" },
-  "3320": { lat: 33.8032, lng: -117.9210, name: "Golden Zephyr" },
-  "3322": { lat: 33.8042, lng: -117.9228, name: "Mickey's PhilharMagic" },
-  "3350": { lat: 33.8060, lng: -117.9215, name: "Luigi's Rollickin' Roadsters" },
-  "3352": { lat: 33.8062, lng: -117.9218, name: "Mater's Junkyard Jamboree" }
+  "7340550b-c14d-4def-80bb-acdb51d49a66": {
+    "lat": 33.8121,
+    "lng": -117.919,
+    "name": "Disneyland Park"
+  },
+  "bcfd1e17-3eab-4203-b597-6257a257d427": {
+    "lat": 33.8105,
+    "lng": -117.919,
+    "name": "Main Street Vehicles"
+  },
+  "faaa8be9-cc1e-4535-ac20-04a535654bd0": {
+    "lat": 33.8143,
+    "lng": -117.9174,
+    "name": "Matterhorn Bobsleds"
+  },
+  "9167db1d-e5e7-46da-a07f-ae30a87bc4c4": {
+    "lat": 33.8152,
+    "lng": -117.9197,
+    "name": "Space Mountain"
+  },
+  "88197808-3c56-4198-a5a4-6066541251cf": {
+    "lat": 33.813,
+    "lng": -117.9175,
+    "name": "Buzz Lightyear Astro Blasters"
+  },
+  "1da85181-bf0f-4ccc-b98e-243142f7347b": {
+    "lat": 33.813,
+    "lng": -117.9165,
+    "name": "Autopia"
+  },
+  "8e686e4c-f3db-4d9c-a185-2d54b1fa8899": {
+    "lat": 33.8137,
+    "lng": -117.9189,
+    "name": "Casey Jr. Circus Train"
+  },
+  "ff52cb64-c1d5-4feb-9d43-5dbd429bac81": {
+    "lat": 33.8113,
+    "lng": -117.9215,
+    "name": "Haunted Mansion"
+  },
+  "1b83fda8-d60e-48e4-9a3d-90ddcbcd1001": {
+    "lat": 33.8129,
+    "lng": -117.9216,
+    "name": "Jungle Cruise"
+  },
+  "90ee50d4-7cc9-4824-b29d-2aac801acc29": {
+    "lat": 33.8134,
+    "lng": -117.918,
+    "name": "Pinocchio's Daring Journey"
+  },
+  "9d401ad3-49b2-469f-ac73-93eb429428fb": {
+    "lat": 33.813,
+    "lng": -117.918,
+    "name": "Mr. Toad's Wild Ride"
+  },
+  "82aeb29b-504a-416f-b13f-f41fa5b766aa": {
+    "lat": 33.8124,
+    "lng": -117.9209,
+    "name": "Pirates of the Caribbean"
+  },
+  "4f0053e7-b8db-4833-b02f-35e1c91b4523": {
+    "lat": 33.8131,
+    "lng": -117.9182,
+    "name": "Snow White's Enchanted Wish"
+  },
+  "cc980e8e-192f-48b6-848c-27784084e54b": {
+    "lat": 33.8138,
+    "lng": -117.9185,
+    "name": "Dumbo the Flying Elephant"
+  },
+  "b2c2549c-e9da-4fdd-98ea-1dcff596fed7": {
+    "lat": 33.815,
+    "lng": -117.923,
+    "name": "Millennium Falcon: Smugglers Run"
+  },
+  "c23af6ba-8515-406a-8a48-d0818ba0bfc9": {
+    "lat": 33.8135,
+    "lng": -117.9182,
+    "name": "Peter Pan's Flight"
+  },
+  "e0cfed11-96d7-40f3-907f-5cfed172592a": {
+    "lat": 33.8133,
+    "lng": -117.9194,
+    "name": "Mad Tea Party"
+  },
+  "e2d460e9-2bef-4613-b126-092ab7cb37e5": {
+    "lat": 33.8115,
+    "lng": -117.9195,
+    "name": "Disneyland Railroad"
+  },
+  "0de1413a-73ee-46cf-af2e-c491cc7c7d3b": {
+    "lat": 33.812,
+    "lng": -117.923,
+    "name": "Big Thunder Mountain Railroad"
+  },
+  "a07f3110-013e-43bb-a182-e66bb8b5e28d": {
+    "lat": 33.8136,
+    "lng": -117.917,
+    "name": "Alice in Wonderland"
+  },
+  "cb929138-d77a-4dd2-983c-f651bbd1bd92": {
+    "lat": 33.814,
+    "lng": -117.918,
+    "name": "Storybook Land Canal Boats"
+  },
+  "64d44aaa-6857-4693-b24b-bcff6c6dcfa1": {
+    "lat": 33.815,
+    "lng": -117.917,
+    "name": "Finding Nemo Submarine Voyage"
+  },
+  "b1d285a7-2444-4a7c-b7bb-d2d4d6428a85": {
+    "lat": 33.805,
+    "lng": -117.9225,
+    "name": "Grizzly River Run"
+  },
+  "8f586a2f-cef5-46d3-b822-fd622c4e9e33": {
+    "lat": 33.8042,
+    "lng": -117.9228,
+    "name": "Mickey's PhilharMagic"
+  },
+  "2295351d-ce6b-4c04-92d5-5b416372c5b5": {
+    "lat": 33.8065,
+    "lng": -117.9195,
+    "name": "WEB SLINGERS: A Spider-Man Adventure"
+  },
+  "c8a4b7b1-c1b2-4dfe-b73c-4e834b4a73db": {
+    "lat": 33.8035,
+    "lng": -117.9215,
+    "name": "Jumpin' Jellyfish"
+  },
+  "86ab3069-110d-49c5-a7e7-29ddf28695a6": {
+    "lat": 33.8045,
+    "lng": -117.921,
+    "name": "Toy Story Midway Mania!"
+  },
+  "46097afe-a1ea-4807-93d3-14d14f36e55f": {
+    "lat": 33.8062,
+    "lng": -117.9218,
+    "name": "Mater's Junkyard Jamboree"
+  },
+  "f44a5072-3cda-4c7c-8574-33ad09d16cca": {
+    "lat": 33.8038,
+    "lng": -117.922,
+    "name": "Goofy's Sky School"
+  },
+  "5d07a2b1-49ca-4de7-9d32-6d08edf69b08": {
+    "lat": 33.804,
+    "lng": -117.922,
+    "name": "Incredicoaster"
+  },
+  "40524fba-5d84-49e7-9204-f493dbe2d5a4": {
+    "lat": 33.8065,
+    "lng": -117.921,
+    "name": "Monsters, Inc. Mike & Sulley to the Rescue!"
+  },
+  "7a09a2f0-e226-4f3e-86f8-2598ab67ec44": {
+    "lat": 33.806,
+    "lng": -117.9215,
+    "name": "Luigi's Rollickin' Roadsters"
+  },
+  "4f5b28d0-b78e-482b-8e2e-1f90756d6220": {
+    "lat": 33.804,
+    "lng": -117.9225,
+    "name": "Silly Symphony Swings"
+  },
+  "c60c768b-3461-465c-8f4f-b44b087506fc": {
+    "lat": 33.8055,
+    "lng": -117.9215,
+    "name": "Radiator Springs Racers"
+  },
+  "10a5fc6f-5ad3-414b-9bdd-e6bae097b6ad": {
+    "lat": 33.8032,
+    "lng": -117.921,
+    "name": "Golden Zephyr"
+  }
 };
